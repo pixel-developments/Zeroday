3d0d67f3f22a366d88aa08742c4e0d66a9ea743d
Zuke <zgaming705@gmail.com>
2020-08-01 08:27:19 -0400

    Made changes to ToDo and RPS

--- commands/rockpaperscissors.js
+++ commands/rockpaperscissors.js
@@ -7,45 +7,52 @@ const answers = [
 ]
 
 exports.run = async (client, message, args, db) => {
-    if(!args[0]) return message.reply(`Invalid Arguments! | ${prefix}rps [rock|paper|scissors]`)
+
+    await db.collection('guild_settings').doc(message.guild.id).get().then(async (q) => {
+        if (q.exists) {
+            prefix = q.data().prefix;
+
+            if(!args[0]) return message.reply(`Invalid Arguments! | ${prefix}rps [rock|paper|scissors]`)
     
-    const random = Math.floor(Math.random() * answers.length);
-    const result = answers[random];
-    const choice = args[0];
+            const random = Math.floor(Math.random() * answers.length);
+            const result = answers[random];
+            const choice = args[0];
 
-    let embed = new MessageEmbed()
-        .setAuthor('Rock, Paper, Scissors', 'https://media2.giphy.com/media/jqI3buvbN3EitBjHfB/giphy.gif')
-        .addField('Your Choice:', choice.toLowerCase())
-        .addField('My Choice', result.toLowerCase());
+            let embed = new MessageEmbed()
+                .setAuthor('Rock, Paper, Scissors', 'https://media2.giphy.com/media/jqI3buvbN3EitBjHfB/giphy.gif')
+                .addField('Your Choice:', choice.toLowerCase())
+                .addField('My Choice', result.toLowerCase());
 
-    if(result.toLowerCase() == choice.toLowerCase()) {
-        embed.setDescription("It's a tie!")
-        return message.channel.send(embed);
-    }
+            if(result.toLowerCase() == choice.toLowerCase()) {
+                embed.setDescription("It's a tie!")
+                return message.channel.send(embed);
+            }
 
-    switch(choice) {
-        case 'rock': {
-            if(result === 'paper') {
-                embed.setDescription("I Won!");
-            } else {
-                embed.setDescription("You Won!")
+            switch(choice) {
+                case 'rock': {
+                    if(result === 'paper') {
+                        embed.setDescription("I Won!");
+                    } else {
+                        embed.setDescription("You Won!")
+                    }
+                }
+                case 'paper': {
+                    if(result === 'scissors') {
+                        embed.setDescription("I Won!");
+                    } else {
+                        embed.setDescription("You Won!")
+                    }
+                }
+                case 'scissors': {
+                    if(result === 'rock') {
+                        embed.setDescription("I Won!");
+                    } else {
+                        embed.setDescription("You Won!")
+                    }
+                }
             }
         }
-        case 'paper': {
-            if(result === 'scissors') {
-                embed.setDescription("I Won!");
-            } else {
-                embed.setDescription("You Won!")
-            }
-        }
-        case 'scissors': {
-            if(result === 'rock') {
-                embed.setDescription("I Won!");
-            } else {
-                embed.setDescription("You Won!")
-            }
-        }
-    }
+    });
 }
 
 exports.conf = {
