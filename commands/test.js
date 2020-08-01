958e32534dbc0b1160c907912ed4eba7f49900ef
Zuke <zgaming705@gmail.com>
2020-08-01 13:10:17 -0400

    Should be good now

--- commands/test.js
+++ commands/test.js
@@ -3,11 +3,9 @@ const { MessageEmbed } = require('discord.js')
 exports.run = async (client, message, args, db) => {
     const errEmbed = new MessageEmbed()
         .setAuthor('Error!', 'https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png')
-        .setDescription('An error occured while preforming this command!')
+        .setDescription('An error occured while preforming this command!\nPlease visit the (Support server)[https://discord.gg/6pjvxpR] to report this!')
         .addField(`Error`, 'Undefined')
         .addField('Description', 'The dipshit developer forgot something')
-        .setFooter('Please report this back to Zuke on the ZeroDay Support server (Click title)')
-        .setURL('https://discord.gg/6pjvxpR')
         .setColor('a81d0d')
         message.channel.send(errEmbed);
 }
