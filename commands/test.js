5cddad16916b620312c7e3eb76e3deee5d7cb7d5
Zuke <zgaming705@gmail.com>
2020-08-01 13:11:27 -0400

    Now we good

--- commands/test.js
+++ commands/test.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require('discord.js')
 exports.run = async (client, message, args, db) => {
     const errEmbed = new MessageEmbed()
         .setAuthor('Error!', 'https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png')
-        .setDescription('An error occured while preforming this command!\nPlease visit the (Support server)[https://discord.gg/6pjvxpR] to report this!')
+        .setDescription('An error occured while preforming this command!\nPlease visit the [Support server](https://discord.gg/6pjvxpR) to report this!')
         .addField(`Error`, 'Undefined')
         .addField('Description', 'The dipshit developer forgot something')
         .setColor('a81d0d')
