c5359157d8f3a121907cc91622e878053a22ccd5
Zuke <zgaming705@gmail.com>
2020-08-03 15:53:07 -0400

    Added NameThatColor

--- cmd_temp.js
+++ cmd_temp.js
@@ -12,6 +12,14 @@ exports.run = async (client, message, args, db) => {
 
             if (q.data().prune === true) message.delete();
         }
+    }).catch(err => {
+        const errEmbed = new MessageEmbed()
+            .setAuthor('Error!', 'https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png')
+            .setDescription('An error occured while preforming this command!\nPlease visit the [Support server](https://discord.gg/6pjvxpR) to report this!')
+            .addField(`Error`, err.name)
+            .addField('Description', err.description)
+            .setColor('a81d0d')
+        message.channel.send(errEmbed);
     });
 
     db.collection('guilds').doc(message.guild.id).get().then(async (q) => {
@@ -23,6 +31,14 @@ exports.run = async (client, message, args, db) => {
             if (!mods.includes(message.member.roles.highest.id) || !admins.includes(message.member.roles.highest.id)) return message.reply("You don't have permission to use this command!");
         
         }
+    }).catch(err => {
+        const errEmbed = new MessageEmbed()
+            .setAuthor('Error!', 'https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png')
+            .setDescription('An error occured while preforming this command!\nPlease visit the [Support server](https://discord.gg/6pjvxpR) to report this!')
+            .addField(`Error`, err.name)
+            .addField('Description', err.description)
+            .setColor('a81d0d')
+        message.channel.send(errEmbed);
     });
 }
 
