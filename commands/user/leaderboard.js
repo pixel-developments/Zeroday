919e0d97baa3c653dcd082ec4747e26331b71c2f
Dragonzuke <zgaming705@gmail.com>
2020-08-05 23:08:41 -0400

    Mess to clean up

--- commands/user/leaderboard.js
+++ commands/user/leaderboard.js
@@ -26,22 +26,28 @@ exports.run = async (client, message, args, db) => {
     let user_id = users.docs.map(doc => doc.data());
 
     let lvl_map = new Map();
+    let lvl_array = [];
     let string = "";
     let index = 1;
 
     for (let doc of user_id) {
         lvl_map.set(doc.id, doc.level);
-        let lvl = new Map([...lvl_map.entries()].sort((a, b) => b[1] - a[1]));
-        console.log(lvl);
+        //let lvl = new Map([...lvl_map.entries()].sort((a, b) => b[1] - a[1]));
+        let lvl = lvl_map.get(doc.id);
+        lvl_array.push(lvl);
+        lvl_array.sort(function(a, b) { return b-a });
+        console.log(lvl_array.map(m => m.level));
 
         let user = message.guild.members.cache.get(doc.id);
         
-        string += `**${index++}.** ${user} | ${lvl.get(doc.id)}\n`;
+        //string += `**${index++}.** ${user} | ${lvl.get(doc.id)}\n`;
+        string += `**${index++}.** ${user} | ${lvl}\n`;
     }
 
     let embed = new MessageEmbed()
         .setAuthor(`${message.guild.name} Level Leaderboard`, message.guild.iconURL)
-        .setDescription(string)
+        //.setDescription(string)
+        .setDescription(lvl_array.map(m => m.level))
         .setColor('#6acf42');
 
     await message.channel.send(embed);
